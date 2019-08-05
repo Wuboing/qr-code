9387d727b0670f00a7bb8d7d00074a7308d2e16e
Wuboing <48124904+Wuboing@users.noreply.github.com>
2019-08-05 14:08:48 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="home">
+    <p>...</p>
+    <p>二维码测试</p>
+    <div>
+      text:<input type="text" v-model="text">
+      logo:<input type="text" v-model="logo">
+      size:<input type="number" v-model="size">
+      <br/>
+      bgclor:<input type="color" v-model="bgColor">
+      color: <input type="color" v-model="color">
+      <br/>
+      <button @click="getData">确定</button>
+      <img :src ="datas"/>
+      <p>{{datas}}</p>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'app',
+    data(){
+      return{
+        text: 'hello,world!',
+        logo: 'https://api.imjad.cn/qrcode/logo.png',
+        size: 200,
+        level: 'Q',
+        bgColor: '#ffffff',
+        color: '#000000',
+        datas: ''
+      }
+    },
+    methods: {
+      getData(){
+        this.datas = 'https://api.imjad.cn/qrcode?text='+this.text+
+                '&logo='+this.logo+'&size='+this.size
+                +'&level=H&bgcolor='+('%23'+this.bgColor.substring(1))+'&fgcolor='+('%23'+this.color.substring(1))
+      }
+    }
+  }
+</script>
 
 <style lang="scss">
 #app {
