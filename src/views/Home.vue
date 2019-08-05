2708cbb74b6ff29cc8901d53e68ad96c71f91222
Wuboing <48124904+Wuboing@users.noreply.github.com>
2019-08-05 13:27:09 +0800

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <p>...</p>
+    <p>二维码测试</p>
+    <div>
+      text:<input type="text" v-model="text">
+      logo:<input type="text" v-model="logo">
+      size:<input type="number" v-model="size">
+      <br/>
+      bgclor:<input type="color" v-model="bgColor">
+      color: <input type="color" v-model="color">
+      <br/>
+      <button @click="getData">确定</button>
+      <img :src ="datas"/>
+      <p>{{datas}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data(){
+    return{
+      text: 'hello,world!',
+      logo: 'https://api.imjad.cn/qrcode/logo.png',
+      size: 200,
+      level: 'Q',
+      bgColor: '#ffffff',
+      color: '#000000',
+      datas: ''
+    }
+  },
+  methods: {
+    getData(){
+      this.datas = 'https://api.imjad.cn/qrcode?text='+this.text+
+              '&logo='+this.logo+'&size='+this.size
+              +'&level=H&bgcolor='+('%23'+this.bgColor.substring(1))+'&fgcolor='+('%23'+this.color.substring(1))
+    }
+  }
+}
+</script>
